7874bd9c58f11d09bb7e7fca661d00267f56b8ac
Karolina Fucikova <karolina.fucikova@gmail.com>
2023-09-04 19:39:54 -0400

    linear models - complete

--- src/linear_models.R
+++ src/linear_models.R
@@ -31,17 +31,43 @@ fitted(blue.lm)
 
 plot(blue)
 lines(spline(blue$Light, fitted(blue.lm)), lwd=2)
+
+### logistic regression
+log <- read.csv("data/log_growth.csv")
+names(log)
+plot(log)
+log.lm <- lm(Growth ~ log(Nutrient), data = log)
+summary(log.lm)
+
+plot(log)
+lines(spline(log$Nutrient, fitted(log.lm)), lwd=2)
+
+plot(Growth ~ log(Nutrient), data = log)
+abline (log.lm)
+
       
 #######################
 # species-area curve model fitting on Solomon Island bird data
+# S = cA^z, or: logS = logc + zlogA
+# excel results: birds = 27.63*area^0.15
+# or: log(birds) = log(27.63) + 0.15*log(area)
+# or: log(birds) = 1.44 + 0.15*log(area)
 solomon <- read.csv("data/Solomon_birds.csv")
 head(solomon)
-# initial parameters
-z <- 0.3
-c <- 10
-# the formula is S = c*A^z
-# or logS = logc + zlogA
-#solomon.lm <- lm(log(Birds)~log(Area)+log(1), data=solomon)
-#fitted(solomon.lm)
-#plot(log(solomon))
-#lines(spline(solomon$Area, fitted(solomon.lm)), lwd=2)
+sol.lm <- lm(Birds ~ log(Area), data = solomon)
+summary(sol.lm)
+
+# birds = 8.1*Ln(Area) + 19.8
+
+plot(solomon)
+lines(spline(solomon$Area, fitted(sol.lm)), lwd=2)
+
+plot(Birds ~ log(Area), data = solomon)
+abline(sol.lm)
+
+# sol.lm2 <- lm(log10(Birds) ~ log10(Area), data = solomon)
+# summary(sol.lm2)
+#plot(log10(Birds) ~ log10(Area), data = solomon)
+# log10(Birds) = 0.18*log10(Area) + 1.37
+# 1.44 vs. 1.37 for log(c) and 0.15 vs. 0.18 for z - NOT BAD R vs. Excel
+
