ea3db10ae2bbc83aa8060a54b511bd655975b5b9
Karolina Fucikova <karolina.fucikova@gmail.com>
2023-11-09 15:12:32 -0500

    Martinez updated code

--- src/Martinez.R
+++ src/Martinez.R
@@ -1,3 +1,4 @@
+library("ggpubr")
 birds <- read.csv("data/Shorebirds_PreyIntake.csv", sep=';')
 inverts <-read.csv("data/Benthic_AbundanceAndBiomass.csv", sep=';')
 head(birds)
@@ -9,3 +10,10 @@ boxplot(MeanLenghtPreyMinute~Treatment*Period, data=birds)
 boxplot(Total_Abundance~Treatment*Period, data=inverts)
 boxplot(Total_Biomass~Treatment*Period, data=inverts)
 boxplot(Polychaeta_Biomass~Treatment*Period, data=inverts)
+
+ggbarplot(
+  inverts, x = "Treatment", y = "Total_Biomass", 
+  add = c("mean_se", "jitter"), 
+  color = "Period",
+  position = position_dodge(0.8),
+  xlab = "Seaweed Cultivation", ylab = "Total Biomass") 
